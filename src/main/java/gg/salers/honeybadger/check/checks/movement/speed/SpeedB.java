ced0d4ddafae204309e2b1b5705d96be4911afff
Orress35 <79585248+Orress35@users.noreply.github.com>
2021-07-28 20:58:02 +0300

    Update SpeedB.java

--- src/main/java/gg/salers/honeybadger/check/checks/movement/speed/SpeedB.java
+++ src/main/java/gg/salers/honeybadger/check/checks/movement/speed/SpeedB.java
@@ -13,36 +13,17 @@ import java.time.LocalDate;
 @CheckData(name = "Speed (B)", experimental = true)
 public class SpeedB extends Check {
 
+    private double lastDeltaXZ;
     private boolean wasOnGround;
-
 
     @Override
     public void onPacket(PacketEvent event, PlayerData playerData) {
         if (event.getPacketType() == PacketType.Play.Client.POSITION || event.getPacketType() == PacketType.Play.Client.POSITION_LOOK) {
+            double lastDeltaXZ = this.lastDeltaXZ;
+            this.lastDeltaXZ = playerData.getMovementProcessor().getDeltaXZ();
             boolean isOnGround = playerData.getBukkitPlayerFromUUID().isOnGround();
             boolean wasOnGround = this.wasOnGround;
             this.wasOnGround = isOnGround;
-            double limit = 0.905;
-            limit += playerData.getBukkitPlayerFromUUID().getWalkSpeed() > 0.2 ? 0 :
-                    playerData.getBukkitPlayerFromUUID().getWalkSpeed();
-            if (playerData.getBukkitPlayerFromUUID().getLocation().add(0, -1.25, 0).getBlock().getType()
-                    == Material.ICE || playerData.getBukkitPlayerFromUUID().getLocation().add(0,-1.25,0).
-                    getBlock().getType() == Material.PACKED_ICE) {
-                limit += 0.6;
-
-            }
-            if(isOnGround && wasOnGround) {
-                limit = 0.4401;
-            }
-            if(!playerData.getBukkitPlayerFromUUID().getLocation().add(0,0.899,0).getBlock().isEmpty()) {
-                limit = 1.31;
-            }
-
-            if(playerData.getMovementProcessor().getDeltaXZ() > limit) {
-                flag(playerData,"l=" + limit + " d=" + playerData.getMovementProcessor().getDeltaXZ());
-            }
         }
-
-
     }
 }
